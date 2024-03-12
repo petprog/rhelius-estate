dab7f5e3cce0c00df6f534189df96ed7647a21bd
petprog <farinupeter@gmail.com>
2024-03-12 15:14:05 +0100

    Complete signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-between items-center max-w-6xl mx-auto py-3 px-4 ">
         {/* Brand name */}
         <Link to="/">
           <h1 className="font-bold text-lg sm:text-xl flex flex-wrap">
@@ -24,14 +24,14 @@ export default function Header() {
           />
           <FaSearch className="text-slate-600" />
         </form>
-        <ul className=" hidden sm:flex sm:gap-4">
-          <li className=" text-slate-700 hover:underline">
+        <ul className=" flex sm:gap-4">
+          <li className="hidden sm:inline text-slate-700 hover:underline">
             <Link to="/">Home</Link>
           </li>
-          <li className=" text-slate-700 hover:underline">
+          <li className=" hidden sm:inline text-slate-700 hover:underline">
             <Link to="/about">About</Link>
           </li>
-          <li className=" text-slate-700 hover:underline">
+          <li className="text-slate-700 hover:underline">
             <Link to="/login">Login</Link>
           </li>
         </ul>
