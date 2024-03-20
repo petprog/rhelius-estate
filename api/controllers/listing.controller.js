0a5af8a11ff1f3da14d4baa1cac8a88ae203d713
petprog <farinupeter@gmail.com>
2024-03-20 09:50:27 +0100

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -24,3 +24,23 @@ export const deleteListing = async (req, res, next) => {
     next(err);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) return next(errorHandler(404, "Listing not found!"));
+    if (req.user.id !== listing.userRef.toString())
+      return next(errorHandler(401, "You can only update your listings"));
+    const result = await Listing.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    const updatedListing = result._doc;
+    res.status(200).send({
+      message: "Listing has been updated.",
+      success: true,
+      data: updatedListing,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,10 +4,12 @@ const router = Router();
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../middlewares/verifyUser.js";
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
